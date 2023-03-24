e7e92ae42fbf20093fd8828b9451f40bd38d9d47
stivenzhang <118372320+stivenzhang@users.noreply.github.com>
2023-03-24 23:44:59 +0100

    sdfa

--- php/DBConnection.php
+++ php/DBConnection.php
@@ -3,11 +3,10 @@ function connection(){
     $host = "database";
     $user = "root";
     $password = 'tiger';
-    $connection = new mysqli($host, $user, $password, "Sistemi_Stiven_Zhang");
+    $database = "Sistemi_Stiven_Zhang";
+    $connection = new mysqli($host, $user, $password, $database);
     if($connection->connect_error){
         die();
     }
-    $query = "CREATE DATABASE Sistemi_Stiven_Zhang, use Sistemi_Stiven_Zhang";
-    $connection->query($query);
     return $connection;
 }
