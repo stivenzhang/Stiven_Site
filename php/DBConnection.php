236f5568a86a01c7a09855776e2e513ac59b0a26
stivenzhang <118372320+stivenzhang@users.noreply.github.com>
2023-03-24 23:40:37 +0100

    dsa

--- php/DBConnection.php
+++ php/DBConnection.php
@@ -3,10 +3,11 @@ function connection(){
     $host = "database";
     $user = "root";
     $password = 'tiger';
-    $database = "Sistemi_Stiven_Zhang";
     $connection = new mysqli($host, $user, $password, $database);
     if($connection->connect_error){
         die();
     }
+    $query = "CREATE DATABASE Sistemi_Stiven_Zhang";
+    $connection->query($query);
     return $connection;
 }
