b50d160ef3397bba261742b91504a588e150263c
stivenzhang <118372320+stivenzhang@users.noreply.github.com>
2023-03-24 23:35:17 +0100

    sfad

--- php/registration.php
+++ php/registration.php
@@ -4,10 +4,8 @@ $connection = connection();
 $username = $_POST['username'];
 $password = $_POST['password'];
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-$query = 'select * from users where users.username = ?';
-$statement = $connection->prepare($query);
-$statement->bind_param("s", $username);
-$queried = $statement->execute();
+$query = "select id from users where users.username = $username";
+$queried = $connection->query($query);
 if(count($queried->fetch_assoc()) == 0){
     $query = "insert into users (users.username, users.password) values (?, ?)";
     $statement = $connection->prepare($query);
