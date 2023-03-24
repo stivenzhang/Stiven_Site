a13341add91b4735707b7b02b86b9d54758fe7ff
stivenzhang <118372320+stivenzhang@users.noreply.github.com>
2023-03-24 23:32:12 +0100

    da

--- php/registration.php
+++ php/registration.php
@@ -4,12 +4,12 @@ $connection = connection();
 $username = $_POST['username'];
 $password = $_POST['password'];
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-$query = "select * from users where username = ?";
+$query = 'select * from users where users.username = ?';
 $statement = $connection->prepare($query);
 $statement->bind_param("s", $username);
 $queried = $statement->execute();
 if(count($queried->fetch_assoc()) == 0){
-    $query = "insert into users (username, password) values (?, ?)";
+    $query = "insert into users (users.username, users.password) values (?, ?)";
     $statement = $connection->prepare($query);
     $statement->bind_param("ss", $username, $hashedPassword);
     $queried = $statement->execute();
