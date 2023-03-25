57ee222de343507af9f1b4c654b0da5d067b6ffe
stivenzhang <118372320+stivenzhang@users.noreply.github.com>
2023-03-25 12:06:35 +0100

    reas

--- php/registration.php
+++ php/registration.php
@@ -9,8 +9,7 @@ $statement = $connection->prepare($query);
 $statement->bind_param("s", $username);
 $statement->execute();
 $queried = $statement->get_result();
-return $queried->fetch_assoc();
-if(count($queried->fetch_assoc()) == 0){
+if(!isset($queried->fetch_assoc()['id'])){
     $query = "insert into users (users.username, users.password) values (?, ?)";
     $statement = $connection->prepare($query);
     $statement->bind_param("ss", $username, $hashedPassword);
@@ -19,5 +18,5 @@ if(count($queried->fetch_assoc()) == 0){
     return "User already registered";
 }
 $connection->close();
-
+header("Location: ../login.php");
 die();
