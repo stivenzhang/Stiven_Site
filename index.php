4b2f0ecdfd995854e09d8404fdc29b80c4c922d0
stivenzhang <118372320+stivenzhang@users.noreply.github.com>
2023-03-25 19:53:39 +0100

    dsafa

--- index.php
+++ index.php
@@ -7,36 +7,70 @@
     <link rel="stylesheet" href="css/general.css">
     <script src="bootstrap/js/bootstrap.js"></script>
 </head>
-<body class="d-flex flex-row">
-<aside class="vh-100 w-25 bg-white">
-    <div class="p-3 overflow-auto sidebar">
-        <p class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+<body class="d-flex flex-row overflow-hidden">
+<aside class="vh-100 w-25 bg-white border-end">
+    <div class="p-3 overflow-auto sidebar border-bottom">
+        <p class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom text-white">
             <span class="fs-5 fw-semibold">What i've done (Stiven_Zhang)</span>
         </p>
         <ul class="list-unstyled ps-0">
             <li class="mb-1">
-                <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#tecnologies-collapse" aria-expanded="false">
+                <button class="text-white btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#technologies-collapse" aria-expanded="false">
                     Technologies
                 </button>
-                <div class="collapse" id="tecnologies-collapse">
-                    <ul class="fw-normal pb-1 small">
-                        <li>IDK</li>
-                        <li>CI DEVO PENSARE</li>
-                        <li>不知道</li>
+                <div class="collapse" id="technologies-collapse">
+                    <ul class="text-white fw-normal pb-1 small">
+                        <li class="rounded collapsed" data-bs-toggle="collapse" data-bs-target="#lamp-collapse" aria-expanded="false">
+                            Lamp
+                        </li>
+                        <div class="collapse" id="lamp-collapse">
+                            <ul class="fw-normal pb-1">
+                                <li class="rounded collapsed" data-bs-toggle="collapse" data-bs-target="#linux" aria-expanded="false">
+                                    Linux(Ubuntu)
+                                </li>
+                                <div class="collapse" id="linux">
+                                    <ul>
+                                        <li>Used for</li>
+                                        <li>Commands</li>
+                                    </ul>
+                                </div>
+                                <li>Apache</li>
+                                <li>Mysql</li>
+                                <li>Php</li>
+                            </ul>
+                        </div>
+                        <li class="rounded collapsed" data-bs-toggle="collapse" data-bs-target="#web-client" aria-expanded="false">
+                            Web client
+                        </li>
+                        <div class="collapse" id="web-client">
+                            <ul class="fw-normal pb-1 small">
+                                <li>Html</li>
+                                <li>Javascript</li>
+                            </ul>
+                        </div>
+                        <li class="rounded collapsed" data-bs-toggle="collapse" data-bs-target="#docker-collapse" aria-expanded="false">
+                            Docker
+                        </li>
+                        <div class="collapse" id="docker-collapse">
+                            <ul class="fw-normal pb-1">
+                                <li>Used for</li>
+                                <li>Commands</li>
+                            </ul>
+                        </div>
+                        <li>Aws</li>
                     </ul>
                 </div>
             </li>
         </ul>
     </div>
-    <div class="account d-flex flex-row">
-        <div class="align-self-center col-6" id="user">
-            <span>User: </span>
+    <div class="account d-flex flex-row p-3">
+        <div class="align-self-center col-6 text-truncate" id="user">
+            Stiven by default
         </div>
-        <button class="btn btn-primary col-6">Sign out</button>
+        <button class="btn btn-primary col-6" id="sign_out">Sign out</button>
     </div>
 </aside>
-
-<main class="w-75 h-100">fdasfsdafasdfsad</main>
-
+<main class="w-75 vh-100 main p-5">
+</main>
 </body>
 </html>
