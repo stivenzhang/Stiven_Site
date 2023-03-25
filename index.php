738ae9cdf3a8b40ee97635e1c38e330dbe4faf20
stivenzhang <118372320+stivenzhang@users.noreply.github.com>
2023-03-25 18:33:49 +0100

    ads

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<?php
-session_start();
-if(!isset($_SESSION['id']))
-    header("Location: login.php");
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,7 +7,36 @@ if(!isset($_SESSION['id']))
     <link rel="stylesheet" href="css/general.css">
     <script src="bootstrap/js/bootstrap.js"></script>
 </head>
-<body>
-afsdasda
+<body class="d-flex flex-row">
+<aside class="vh-100 w-25 bg-white">
+    <div class="p-3 overflow-auto sidebar">
+        <p class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+            <span class="fs-5 fw-semibold">What i've done (Stiven_Zhang)</span>
+        </p>
+        <ul class="list-unstyled ps-0">
+            <li class="mb-1">
+                <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#tecnologies-collapse" aria-expanded="false">
+                    Technologies
+                </button>
+                <div class="collapse" id="tecnologies-collapse">
+                    <ul class="fw-normal pb-1 small">
+                        <li>IDK</li>
+                        <li>CI DEVO PENSARE</li>
+                        <li>不知道</li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="account d-flex flex-row">
+        <div class="align-self-center col-6" id="user">
+            <span>User: </span>
+        </div>
+        <button class="btn btn-primary col-6">Sign out</button>
+    </div>
+</aside>
+
+<main class="w-75 h-100">fdasfsdafasdfsad</main>
+
 </body>
 </html>
