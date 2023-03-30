37e19be13904ced750d159f41a8a28985c967c99
stivenzhang <118372320+stivenzhang@users.noreply.github.com>
2023-03-30 20:38:33 +0200

    asd

--- javascript/logged.js
+++ javascript/logged.js
@@ -10,7 +10,15 @@ if(document.title === "How to do")
                     if(response.status === 200){
                         user.innerHTML = response.response
                         if(response.response === "Ruben1=2" || response.response === "Daniele7")
-                            body_information.innerHTML += "a=b   a^2=b^2   b^2-a^2=b^2-ab   (b-a)(b+a)= b(b-a)   b+a=b   2b=b   2=1"
+                            body_information.innerHTML +=
+                                "<p>a=b</p>" +
+                                "<p>a^2=b^2</p>" +
+                                "<p>b^2-a^2=b^2-ab</p>" +
+                                "<p>(b-a)(b+a)= b(b-a)</p>" +
+                                "<p>b+a=b</p>" +
+                                "<p>2b=b</p>" +
+                                "<p>2=1</p>" +
+                                "<p class='text-primary fw-bold'>Quindi prof da questo si può dire palesemente che 1=2 quindi la matematica è tutto fake</p>"
                     }else{
                         user.innerHTML = "IDK WHAT YOU DID BUT FU"
                     }
