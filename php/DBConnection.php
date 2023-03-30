92659953c67912af5276c9167b57521e236a7c44
stivenzhang <118372320+stivenzhang@users.noreply.github.com>
2023-03-30 16:03:50 +0200

    asd

--- php/DBConnection.php
+++ php/DBConnection.php
@@ -1,12 +1,12 @@
 <?php
 function connection(){
     //For aws
-    //$host = "database";
-    $host = "localhost";
+    $host = "database";
+    //$host = "localhost";
     $user = "root";
     //For aws
-    //$password = 'tiger';
-    $password = 'root';
+    $password = 'tiger';
+    //$password = 'root';
     $database = "Sistemi_Stiven_Zhang";
     $connection = new mysqli($host, $user, $password, $database);
     if($connection->connect_error)
