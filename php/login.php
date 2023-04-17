3de97c42b22f82f91b4480ae31c728c2c92514db
stivenzhang <118372320+stivenzhang@users.noreply.github.com>
2023-04-17 11:13:08 +0200

    passsssssss

--- php/login.php
+++ php/login.php
@@ -3,7 +3,7 @@ include_once  "DBConnection.php";
 $connection = connection();
 $username = mysqli_real_escape_string($connection, $_POST['username']);
 $password = mysqli_real_escape_string($connection, trim($_POST['password']).md5("sha256md5"));
-$hashedPassword = hash("sha256", $password);
+$hashedPassword = password_hash($password, PASSWORD_BCRYPT, ['cost' => 13,]);
 $query = "select * from users where username like ? and password like ?";
 $statement = $connection->prepare($query);
 $statement->bind_param("ss", $username, $hashedPassword);
